18244bd3846a3098851b2abe96944705eca40042
ricardorompar <ricardo.romero@hashicorp.com>
2024-09-05 16:58:53 +0200

    changed file provisioner

--- main.tf
+++ main.tf
@@ -139,7 +139,7 @@ resource "null_resource" "configure-cat-app" {
   }
 
   provisioner "file" {
-    source      = "https://github.com/ricardorompar/terraform-r2-hashicat/blob/main/files/deploy_app.sh" #please don't do this in production
+    source      = "https://github.com/ricardorompar/terraform-r2-hashicat/blob/main/files" #please don't do this in production
     destination = "/home/ubuntu/"
 
     connection {

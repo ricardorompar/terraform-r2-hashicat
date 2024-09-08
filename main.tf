7d5e7ac118ccd6e34a339e921412e4d29135181a
ricardorompar <ricardo.romero@hashicorp.com>
2024-09-09 00:49:44 +0200

    ssh key has workspace name

--- main.tf
+++ main.tf
@@ -178,7 +178,7 @@ resource "tls_private_key" "hashicat" {
 }
 
 locals {
-  private_key_filename = "${var.prefix}-ssh-key.pem"
+  private_key_filename = "${terraform.workspace}-ssh-key.pem"
 }
 
 resource "aws_key_pair" "hashicat" {
